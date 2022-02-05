e853ac88da9c72b01633caa73aac1492a09116e1
Anas CHAHID <anas.chahid777@gmail.com>
2022-02-05 20:39:57 +0100

    added mobile support to checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -26,9 +26,11 @@ const CheckoutPage = ({cartItems,total}) =>(
                 <span>Remove</span>
             </div>
         </div>
+        
         {
             cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
         }
+        
         <div className="total">
             <span>TOTAL: ${total}</span>
         </div>
